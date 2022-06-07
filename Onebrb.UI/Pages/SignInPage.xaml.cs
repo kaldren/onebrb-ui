8ac7e8c32729612631301a7c5fff24e93d9dd216
Kaloyan Drenski <drenski666@gmail.com>
2022-06-07 20:51:47 +0300

    Added sign in.

--- Onebrb.UI/Pages/SignInPage.xaml.cs
+++ Onebrb.UI/Pages/SignInPage.xaml.cs
@@ -1,3 +1,6 @@
+using Microsoft.Identity.Client;
+using Onebrb.UI.Core.Auth;
+
 namespace Onebrb.UI.Pages;
 
 public partial class SignInPage : ContentPage
@@ -7,8 +10,39 @@ public partial class SignInPage : ContentPage
 		InitializeComponent();
 	}
 
-    private void signInBtn_Clicked(object sender, EventArgs e)
+    private async void signInBtn_Clicked(object sender, EventArgs e)
     {
-        Navigation.PushAsync(new MainTabbedPage());
+        try
+        {
+            signInStatusLabel.Text = "Please wait a moment...";
+
+            var userContext = await B2CAuthenticationService.Instance.SignInAsync();
+            var token = userContext.AccessToken;
+
+            // Get data from API
+            HttpClient client = new HttpClient();
+            HttpRequestMessage message = new HttpRequestMessage(HttpMethod.Get, App.ApiEndpoint);
+            message.Headers.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
+            HttpResponseMessage response = await client.SendAsync(message);
+            string responseString = await response.Content.ReadAsStringAsync();
+            if (response.IsSuccessStatusCode)
+            {
+                //lblApi.Text = $"Response from API {App.ApiEndpoint} | {responseString}";
+                Navigation.PushAsync(new MainTabbedPage());
+            }
+            else
+            {
+                //lblApi.Text = $"Error calling API {App.ApiEndpoint} | {responseString}";
+            }
+        }
+        catch (MsalUiRequiredException ex)
+        {
+            await DisplayAlert($"Session has expired, please sign out and back in.", ex.ToString(), "Dismiss");
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert($"Exception:", ex.ToString(), "Dismiss");
+        }
+
     }
 }
